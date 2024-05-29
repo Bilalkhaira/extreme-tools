9fbaedaf66195f96f23bd2f9469c452485580832
Bilal <bilalkhaira8989@gmail.com>
2024-05-29 16:14:10 +0500

    sss

--- resources/views/layout/_auth.blade.php
+++ resources/views/layout/_auth.blade.php
@@ -38,7 +38,7 @@
 
             <!--begin::Aside-->
             {{-- <div class="d-flex flex-lg-row-fluid w-lg-50 bgi-size-cover bgi-position-center order-1 order-lg-2" style="background-image: url({{ image('misc/auth-bg.png') }})"> --}}
-                <div class="d-flex flex-lg-row-fluid w-lg-50 bgi-size-cover bgi-position-center order-1 order-lg-2" style="background: linear-gradient(to left, #58126A, #F6B2E1);">
+                <div class="d-flex flex-lg-row-fluid w-lg-50 bgi-size-cover bgi-position-center order-1 order-lg-2" style="background-image:linear-gradient(289deg, #58126A -40.56%, #F6B2E1 119.32%);">
                 <!--begin::Content-->
                 <div class="d-flex flex-column flex-center py-7 py-lg-15 px-5 px-md-15 w-100">
                     <!--begin::Logo-->
